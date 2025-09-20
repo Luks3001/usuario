743b1a42f218a210ee361f1519f1d609d8d3554f
Lucas <lucass3001@gmail.com>
2025-09-20 16:22:38 -0300

    adicionado método que salva dados de usuario

---
+++ src/main/java/com/lucas/usuario/business/dto/EnderecoDTO.java
@@ -0,0 +1,21 @@
+package com.lucas.usuario.business.dto;
+
+import jakarta.persistence.Column;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EnderecoDTO {
+
+    private String rua;
+    private Long numero;
+    private String complemento;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+
+}
