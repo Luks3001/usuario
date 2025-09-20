7e69b1c1937e399c059e6320be4e53f35ca22ad5
Lucas <lucass3001@gmail.com>
2025-09-20 17:04:23 -0300

    adicionados metodos get, delete e login

--- src/main/java/com/lucas/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/lucas/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,7 @@
 package com.lucas.usuario.infrastructure.repository;
 
-import com.lucas.aprendendospring.infrastructure.entity.Endereco;
+
+import com.lucas.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/lucas/usuario/infrastructure/repository/TelefoneRepository.java
+++ src/main/java/com/lucas/usuario/infrastructure/repository/TelefoneRepository.java
@@ -1,6 +1,7 @@
 package com.lucas.usuario.infrastructure.repository;
 
-import com.lucas.aprendendospring.infrastructure.entity.Telefone;
+
+import com.lucas.usuario.infrastructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
