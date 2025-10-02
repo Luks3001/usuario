5f696bbc4c00373f3fea1fe0e2808600b31f0d5c
Lucas <lucass3001@gmail.com>
2025-10-02 15:33:21 -0300

    Adicionado docker

---
+++ src/main/java/com/lucas/usuario/controller/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.lucas.usuario.controller;
+
+import com.lucas.usuario.infrastructure.exceptions.ConflictException;
+import com.lucas.usuario.infrastructure.exceptions.ResourceNotFoundException;
+import com.lucas.usuario.infrastructure.exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handlerResourceNotFoundException(ResourceNotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> handlerConflictException (ConflictException ex){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handlerUnauthorizedException (UnauthorizedException ex){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.UNAUTHORIZED);
+    }
+
+
+}
